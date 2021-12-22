0596fe9fd0070cb1a1599605f4d39c7e690e2db7
lonbar <9698029+lonbar@users.noreply.github.com>
2021-12-22 16:11:23 +0000

    Moved defaults and constants to YAML file.

--- background.py
+++ background.py
@@ -1,25 +1,36 @@
 import numpy as np
+import yaml
+
 from scipy import integrate
 
-def E_z(z, ΩM=0.32, ΩDE=0.68, ΩR=0., w0=-1., wa=0.):
+# Load all the default values / constants from YAML file
+with open("cosmology-constants.yaml", "r") as constantslist:
+    constants = yaml.load(constantslist, Loader=yaml.FullLoader)
+
+def E_z(z, ΩM=constants['matter-density'], ΩDE=constants['DE-density'], 
+        ΩR=constants['rad-density'], w0=constants['w0'], wa=constants['wa']):
     """
     Method to compute the adimensional Hubble rate in the w0waCDm cosmology
     """
     ΩK = 1-ΩM-ΩDE-ΩR
     return np.sqrt(ΩM*(1+z)**3+ΩR*(1+z)**4+ΩDE*(1+z)**(3*(1+w0+wa))*np.exp(-3*wa*z/(1+z))+ΩK*(1+z)**2)
 
-def H_z(z, H0=67, ΩM=0.32, ΩDE=0.68, ΩR=0., w0=-1., wa=0.):
+def H_z(z, H0=constants['Hubble0'], ΩM=constants['matter-density'], 
+        ΩDE=constants['DE-density'], ΩR=constants['rad-density'], 
+        w0=constants['w0'], wa=constants['wa']):
     """
     Method to compute the Hubble rate in the w0waCDm cosmology
     """
     return H0*E_z(z, ΩM, ΩDE, ΩR, w0, wa)
 
-def comoving_distance(z, H0=67, ΩM=0.32, ΩDE=0.68, ΩR=0., w0=-1., wa=0.):
+def comoving_distance(z, H0=constants['Hubble0'], ΩM=constants['matter-density'], 
+                      ΩDE=constants['DE-density'], ΩR=constants['rad-density'], 
+                      w0=constants['w0'], wa=constants['wa']):
     """
     Method to compute the comoving distance
     """
     integrand = lambda x: 1/E_z(x, ΩM, ΩDE, ΩR, w0, wa)
     int, err = integrate.quad(integrand, 0, z)
-    c0 = 2.99792458e5
-    #TODO: this is not beautiful, maybe we could define some global constants
+    c0 = constants['speed-of-light']
     return c0/H0*int
+

70610c81aea2c49b8c846182522a4f8c18170210
lonbar <9698029+lonbar@users.noreply.github.com>
2021-12-22 17:49:07 +0000

    Added transverse comoving distance and angular diameter distance.

--- background.py
+++ background.py
@@ -34,3 +34,30 @@ def comoving_distance(z, H0=constants['Hubble0'], ΩM=constants['matter-density'
     c0 = constants['speed-of-light']
     return c0/H0*int
 
+def transverse_comoving_distance(z, H0=constants['Hubble0'], ΩM=constants['matter-density'],
+                                 ΩDE=constants['DE-density'], ΩR=constants['rad-density'],
+                                 w0=constants['w0'], wa=constants['wa']):
+    """
+    Compute the transverse comoving distance
+    """
+    D_c = comoving_distance(z, H0, ΩM, ΩDE, ΩR, w0, wa)
+    ΩK = 1 - ΩM - ΩDE - ΩR
+    c0 = constants['speed-of-light']
+    x = np.sqrt(ΩK + 0j) * D_c / (c0 / H0)
+    if ΩDE == 0:
+        D_m = c0 / H0 * 2 * (2 - ΩM(1-z) - (2-ΩM)*np.sqrt(1+ΩM*z)) / (ΩM**2*(1+z))
+    else:
+        D_m = D_c * np.sinc(x / np.pi)
+    #elif ΩK <= 0:
+    #    D_m = D_c * np.sinh(x) / x
+    #elif ΩK >= 0:
+    #    D_m = D_c * np.sin(x) / x
+    return D_m.real
+
+def angular_diameter_distance(z, H0=constants['Hubble0'], ΩM=constants['matter-density'],
+                              ΩDE=constants['DE-density'], ΩR=constants['rad-density'],
+                              w0=constants['w0'], wa=constants['wa']):
+    """
+    Computer the angular diameter distance
+    """
+    return transverse_comoving_distance(z, H0, ΩM, ΩDE, ΩR, w0, wa) / (1 + z)

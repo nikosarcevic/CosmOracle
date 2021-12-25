aeca3ba665e3274d225ce27d20f17ec4f86afe9e
Matthijs van der Wild <primarius@gmail.com>
2021-12-25 14:16:34 +0000

    Reversed conditions for closed and open universes.

--- background.py
+++ background.py
@@ -48,14 +48,14 @@ def transverse_comoving_distance(z, H0=constants['Hubble0'], ΩM=constants['matt
     D_c = comoving_distance(z, H0, ΩM, ΩDE, ΩR, w0, wa)
     ΩK = 1 - ΩM - ΩDE - ΩR
     c0 = constants['speed-of-light']
-    x = np.sqrt(ΩK + 0j) * D_c / (c0 / H0)
+    x = np.sqrt(-ΩK + 0j) * D_c / (c0 / H0)
     if ΩDE == 0:
         D_m = c0 / H0 * 2 * (2 - ΩM(1-z) - (2-ΩM)*np.sqrt(1+ΩM*z)) / (ΩM**2*(1+z))
     else:
         D_m = D_c * np.sinc(x / np.pi)
-    #elif ΩK <= 0:
+    #if ΩK >= 0:
     #    D_m = D_c * np.sinh(x) / x
-    #elif ΩK >= 0:
+    #if ΩK <= 0:
     #    D_m = D_c * np.sin(x) / x
     return D_m.real
 

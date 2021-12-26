cfcaeb7b15c71ecfb32bd4a4cbeee6e248dcc862
Nikolina Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2021-12-26 23:40:58 +0000

    plot updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,13 +72,13 @@ st.sidebar.warning(
 
 if z_value:
     st.write('comoving distance is:', round(bg.comoving_distance(float(z_value), H0=float(H0_value), ΩM=float(ΩM_value), ΩDE=float(ΩDE_value), ΩR=float(ΩR_value), w0=float(w0_value) , wa=float(wa_value) ), sig_digits), 'Mpc')
-    z_array = np.linspace(0,float(z_value), 300)
+    z_array = np.linspace(0, float(z_value), 300)
     
-fig = plt.plot(figsize=(5,5))
-plt.plot(z_array, bg.comoving_distance(z_array, H0=float(H0_value), ΩM=float(ΩM_value), ΩDE=float(ΩDE_value), ΩR=float(ΩR_value), w0=float(w0_value) , wa=float(wa_value) ))
-plt.xlabel("z")
-plt.ylabel("r(z)")
+    fig = plt.figure(figsize=(10,10))
+    plt.plot(z_array, bg.comoving_distance(z_array, H0=float(H0_value), ΩM=float(ΩM_value), ΩDE=float(ΩDE_value), ΩR=float(ΩR_value), w0=float(w0_value) , wa=float(wa_value) ))
+    plt.xlabel("z")
+    plt.ylabel("r(z)")
 
-st.pyplot(myfig)
+    st.pyplot(fig)
 
     

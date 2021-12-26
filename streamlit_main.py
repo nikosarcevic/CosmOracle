2bb5171c41da823c63d6ce34083acfdad9f2394d
Marco Bonici <58727599+marcobonici@users.noreply.github.com>
2021-12-26 23:15:10 +0100

    Added streamlit main

---
+++ streamlit_main.py
@@ -0,0 +1,68 @@
+# %%
+import numpy as np
+import streamlit as st
+from scipy import integrate
+import background as bg
+
+
+H0=67
+ΩM=0.32
+ΩR=0
+ΩDE=0.68
+w0=-1.
+wa=0
+speed_of_light=2.99792458e5
+
+st.set_page_config(page_title='CosmoCalc')
+
+logo, name = st.sidebar.columns(2)
+#with logo:
+    #image = 'https://raw.githubusercontent.com/rdzudzar/DistributionAnalyser/main/images/logo_da.png?token=AIAWV2ZRCFKYM42DVFTD3OLAN3CQK'
+    #st.image(image, use_column_width=True)
+with name:
+    st.markdown("<h1 style='text-align: left; color: orange;'> \
+                CosmoCalc </h1>", unsafe_allow_html=True)
+
+st.sidebar.write(" ")
+
+
+
+
+
+distance_value = st.sidebar.text_input('Distance') 
+
+z_value = st.sidebar.text_input('Redshift')
+H0_value = st.sidebar.text_input('Hubble', str(H0))
+ΩM_value = st.sidebar.text_input('Matter Density', str(ΩM))
+ΩDE_value = st.sidebar.text_input('Dark Energy Density', str(ΩDE))
+ΩR_value = st.sidebar.text_input('Radiation Density', str(ΩR))
+w0_value = st.sidebar.text_input('w0', str(w0))
+wa_value = st.sidebar.text_input('wa', str(wa))
+
+sig_digits = int(st.sidebar.text_input('Significant Digits', str(4)))
+
+  
+
+
+# Write About
+st.sidebar.header("About")
+st.sidebar.warning(
+                """
+                CosmoCalc app is created and maintained by 
+                **Marco Bonici, Niko Sarcevic and Matthijs van der Wild**. If you like this app please star its
+                [**GitHub**](ADD URL HERE)
+                repo, share it and feel free to open an issue if you find a bug 
+                or if you want some additional features.
+                """)
+
+
+
+
+if distance_value :
+    st.write('The distance modulus is:', round(bg.calculate_distance_modulus(float(distance_value)), sig_digits), '[no units]')
+    
+ 
+
+if z_value:
+    st.write('comoving distance is:', round(bg.comoving_distance(float(z_value), H0=float(H0_value), ΩM=float(ΩM_value), ΩDE=float(ΩDE_value), ΩR=float(ΩR_value), w0=float(w0_value) , wa=float(wa_value) ), sig_digits), 'Mpc')
+    

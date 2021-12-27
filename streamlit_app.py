32b78d6e2b9b405cd0962f89df6cd9e4535a484d
Nikolina Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2021-12-27 00:23:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,8 +60,7 @@ if z_value:
     plot_DAz = st.checkbox('Plot Angular Diameter Distance')
 
     if plot_rz or plot_DLz or plot_DAz:
-        width = st.slider("plot width", 1, 25, 10)
-        height = st.slider("plot height", 1, 25, 10)
+        
         fig, ax = plt.subplots(figsize=(width, height))
     
         #fig = plt.subplots(figsize=(width, height))
@@ -82,5 +81,8 @@ if z_value:
         ax.legend()
 
         st.pyplot(fig)
+        
+        width = st.slider("plot width", 1, 25, 10)
+        height = st.slider("plot height", 1, 25, 10)
 
     

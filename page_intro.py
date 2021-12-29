190245ee9c59ea2cfeaa2f71baca8f142ac5d358
Nikolina Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2021-12-29 02:12:43 +0000

    Update page_intro.py

--- page_intro.py
+++ page_intro.py
@@ -51,3 +51,15 @@ st.title('References')
 st.markdown('D. Hogg, *Distance Measures in Cosmology*, 2000 [astro-ph/9905116v4](https://arxiv.org/abs/astro-ph/9905116)')
 
 
+# About
+st.sidebar.header("About")
+st.sidebar.markdown(
+                """
+                CosmΩracle app is created and maintained by 
+                [**Marco Bonici**](https://github.com/marcobonici), [**Niko Sarcevic**](https://github.com/nikosarcevic) and [**Matthijs van der Wild**](https://github.com/lonbar). If you like this app please star its
+                [**GitHub**](https://github.com/nikosarcevic/CosmoCompute/)
+                repo, share it and feel free to open an issue if you find a bug 
+                or if you want some additional features.
+                """)
+
+

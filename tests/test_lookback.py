03cf8ecc3af775d5fdd9efcb96ff5491db09a82a
Marco Bonici <58727599+marcobonici@users.noreply.github.com>
2021-12-29 23:13:35 +0100

    Added lookback test

---
+++ tests/test_lookback.py
@@ -0,0 +1,15 @@
+import unittest
+import background as bg
+
+class LookbackTest(unittest.TestCase):
+    def test_redshift(self):
+        data = 1.
+        result = bg.lookback_time(data)
+        self.assertAlmostEqual(result, 7.977 , delta=0.1)
+    def test_redshift_int(self):
+        data = 1
+        result = bg.lookback_time(data)
+        self.assertAlmostEqual(result, 7.977, delta=0.1)
+    def test_redshift_negative(self):
+        data = -1.
+        self.assertRaises(TypeError, bg.lookback_time, data)

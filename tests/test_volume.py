36088bb2cb640bbe993b84fe14145f71809d3180
Marco Bonici <58727599+marcobonici@users.noreply.github.com>
2021-12-29 23:00:33 +0100

    Fixedtest

--- tests/test_volume.py
+++ tests/test_volume.py
@@ -5,11 +5,11 @@ class ComovingVolumeTest(unittest.TestCase):
     def test_redshift(self):
         data = 1.
         result = bg.comoving_volume(data)
-        self.assertAlmostEqual(result, 1.663466e12 , delta=0.1)
+        self.assertAlmostEqual(1e-9*result, 166.3466 , delta=0.1)
     def test_redshift_int(self):
         data = 1
         result = bg.comoving_volume(data)
-        self.assertAlmostEqual(result, 1.663466e12, delta=0.1)
+        self.assertAlmostEqual(1e-9*result, 166.3466, delta=0.1)
     def test_redshift_negative(self):
         data = -1.
         self.assertRaises(TypeError, bg.comoving_volume, data)

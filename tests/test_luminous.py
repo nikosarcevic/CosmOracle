fbe59db6f8c5e5101c0a674ab15cf014eb5d5a77
Marco Bonici <58727599+marcobonici@users.noreply.github.com>
2021-12-29 22:53:58 +0100

    Added luminosity distance test

---
+++ tests/test_luminous.py
@@ -0,0 +1,15 @@
+import unittest
+import background as bg
+
+class ComovingDistanceTest(unittest.TestCase):
+    def test_redshift(self):
+        data = 1.
+        result = bg.luminosity_distance(data)
+        self.assertAlmostEqual(result, 6823.4666, delta=0.1)
+    def test_redshift_int(self):
+        data = 1
+        result = bg.luminosity_distance(data)
+        self.assertAlmostEqual(result, 6823.4666, delta=0.1)
+    def test_redshift_negative(self):
+        data = -1.
+        self.assertRaises(TypeError, bg.luminosity_distance, data)

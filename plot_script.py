f999b585ffcd32e7f4c064848b3f8c9d49ed8fcc
Nikolina Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2021-12-30 01:01:59 +0000

    added a script for plotting the comoving volume

--- plot_script.py
+++ plot_script.py
@@ -117,3 +117,56 @@ def plot_distances_log(plot_rz, plot_trz, plot_DLz, plot_DAz, z_array, rz_array,
       
         return fig
 
+
+def plot_comoving_volume_lin(plot_VCz, z_array, rz_array, VCz_array, width, height):
+        
+        '''
+        Plot the calculated comoving volume as a function of redshift in a linear scale.
+        '''
+        
+        fig, ax = plt.subplots(figsize=(width, height))
+        
+        colors = {
+         'orange' : '#ffc345',
+         'gray' : '#333333',
+         'white' : '#FFFFFF',
+        }
+        
+        ax.spines['bottom'].set_color(colors['orange'])
+        ax.spines['top'].set_color(colors['orange']) 
+        ax.spines['right'].set_color(colors['orange'])
+        ax.spines['left'].set_color(colors['orange'])
+
+        ax.tick_params(axis='x', colors=colors['orange'])
+        ax.tick_params(axis='y', colors=colors['orange'])
+
+        ax.yaxis.label.set_color(colors['orange'])
+        ax.xaxis.label.set_color(colors['orange'])
+        
+        for axis in ['top','bottom','left','right']:
+            ax.spines[axis].set_linewidth(3)
+
+        ax.tick_params(width=3)
+        ax.tick_params(axis='both', labelsize=12)
+
+        ax.set_facecolor(colors['white'])
+        fig.patch.set_facecolor(colors['white'])
+   
+        if plot_VCz:
+            ax.plot(z_array, VCz_array, label='Comoving Volume', color=colors['gray'], ls='-', lw=3)
+        
+
+        legend = plt.legend(frameon = 1)
+        plt.setp(legend.get_texts(), color=colors['gray'])
+        frame = legend.get_frame()
+        frame.set_facecolor(colors['white'])
+        frame.set_edgecolor(colors['white'])
+
+        ax.set_xlabel('Redshift', size=15)
+        ax.set_ylabel('Comoving Volume [$\mathrm{Gpc^3}$]', size=15)
+        
+        return fig
+
+
+
+

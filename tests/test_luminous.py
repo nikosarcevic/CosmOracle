c2736172c208471be384020c50a1d8f2476f64fa
Matthijs van der Wild <primarius@gmail.com>
2022-01-03 13:47:38 +0000

    Updated tests, dropped python 3.6 testing.

--- tests/test_luminous.py
+++ tests/test_luminous.py
@@ -4,12 +4,12 @@ import background as bg
 class LuminosityDistanceTest(unittest.TestCase):
     def test_redshift(self):
         data = 1.
-        result = bg.luminosity_distance(data)
+        result = bg.get_luminosity_distance(data)
         self.assertAlmostEqual(result, 6823.4666, delta=0.1)
     def test_redshift_int(self):
         data = 1
-        result = bg.luminosity_distance(data)
+        result = bg.get_luminosity_distance(data)
         self.assertAlmostEqual(result, 6823.4666, delta=0.1)
     def test_redshift_negative(self):
         data = -1.
-        self.assertRaises(TypeError, bg.luminosity_distance, data)
+        self.assertRaises(TypeError, bg.get_luminosity_distance, data)
